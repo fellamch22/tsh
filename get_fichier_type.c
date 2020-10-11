14c411763217404dc3765c3855c888d5c5677531
Li-FangSu <su.lifang@outlook.com>
2020-10-11 21:55:46 +0200

    update get_fichier_type

--- get_fichier_type.c
+++ get_fichier_type.c
@@ -1,33 +1,51 @@
 #include <string.h>
-#include <unistd.h>
-#include <strings.h>
 #include <stdio.h>
+#include "tar.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include "tar.h"
 
-
-
-
-char get_fichier_type(int fd, char * chemin){
+char get_fichier_type(int fd, char *chemin){
+    struct stat buff;
+    fstat(fd, &buff);
+    int nb_blocks = (buff.st_size + 512 - 1) / 512;
+    //nb blocks exitant dans le fichier.tar
     
     struct posix_header ph;
-    read(fd, &ph, BLOCKSIZE);
 
-    if(fd == -1){
-      perror("open failed");
-      exit(1);
+    int i = 0;
+    while (i < nb_blocks - 2)
+    {
+        bzero(&ph, sizeof(ph));
+        read(fd, &ph, sizeof(ph));
+
+        if (!strcmp(ph.name, chemin)) {
+            if (ph.typeflag == '0')
+                printf("Le chemin mene a un fichier");
+            else if (ph.typeflag == '5')
+                printf("Le chemin mene a un directory");
+            else
+                printf("Le chemin mene a un autre type");
+            return (ph.typeflag);
+        }
+        i++;
+    }
+    printf("Le chemin n'existe pas\n");
+    return 0;
+}
+
+int main(int argc, char * argv[])
+{
+    if (argc != 3)
+    {
+        printf("Wrong number of arguments\nUsage: ./listar <file.tar> <chemin>\n");
+        return 1;
     }
 
-    if (ph.typeflag == '0') {
-        printf("Type[0] : fichier ordinaire\n");
-    }else if (ph.typeflag == '5') {
-        printf("Type[5] : directory\n");   
-    }else{printf("Type[%d] : autre\n", ph.typeflag);
-    }
-
+    int fd = open(argv[1], O_RDONLY);
+    char ret = get_fichier_type(fd, argv[2]);
     close(fd);
 
+    return 0;
 }
