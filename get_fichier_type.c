f4905571c26c413cfa76e2eb37814ef6de26e046
Li-FangSu <su.lifang@outlook.com>
2020-10-11 22:03:59 +0200

    update get_fichier_type

--- get_fichier_type.c
+++ get_fichier_type.c
@@ -15,10 +15,14 @@ char get_fichier_type(int fd, char *chemin){
     struct posix_header ph;
 
     int i = 0;
-    while (i < nb_blocks - 2)
-    {
+    int j = 0;
+    while (i < nb_blocks - 2){
         bzero(&ph, sizeof(ph));
-        read(fd, &ph, sizeof(ph));
+        j = read(fd, &ph, sizeof(ph));
+        if (j == -1){
+            perror("read\n");
+            return ;
+        }
 
         if (!strcmp(ph.name, chemin)) {
             if (ph.typeflag == '0')
@@ -35,16 +39,15 @@ char get_fichier_type(int fd, char *chemin){
     return 0;
 }
 
-int main(int argc, char * argv[])
-{
-    if (argc != 3)
-    {
-        printf("Wrong number of arguments\nUsage: ./listar <file.tar> <chemin>\n");
+int main(int argc, char * argv[]){
+    if (argc != 3){
+        perror("Wrong number of arguments : ");
         return 1;
     }
 
     int fd = open(argv[1], O_RDONLY);
     char ret = get_fichier_type(fd, argv[2]);
+    
     close(fd);
 
     return 0;
