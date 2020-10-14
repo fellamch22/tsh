04eff8743e5fa7fcc722a6e9fdc9f80d20ed92e9
Li-FangSu <su.lifang@outlook.com>
2020-10-15 00:37:45 +0200

    Update afficher_repertoire

--- afficher_repertoire.c
+++ afficher_repertoire.c
@@ -7,50 +7,53 @@
 #include <fcntl.h>
 
 int ft_strlen(char *s) {
-	int i = 0;
-	while(s[i])
-		i++;
-	return i;
+    int i = 0;
+    while(s[i])
+        i++;
+    return i;
 }
 
 void afficher_repertoire(int fd, off_t position){
+    if (fd == -1){
+        perror("open");
+    }
+
     struct stat buff;
     fstat(fd, &buff);
-    int nb_blocks = (buff.st_size + 512 - 1) / 512;
-    
+    // Ici on calcule le nombre de blocks qu'il y a dans le fichier.tar
+    // tel que nbBlock * BLOCKSIZE = taille du fichier
+    // <==> nbBlock = taille de fichier / BLOCKSIZE
+
+    int nb_blocks = (buff.st_size + BLOCKSIZE - 1) / BLOCKSIZE;
     struct posix_header ph;
 
     off_t i = 0;
-	while (i < position){
-		bzero(&ph, sizeof(ph));
-		if (read(fd, &ph, sizeof(ph)) == -1) {
-			perror("read : ");
-			return ;
-		}
-		i++;
-	}
+    while (i < position){
+        bzero(&ph, sizeof(ph));
+        if (read(fd, &ph, sizeof(ph)) == -1) {
+            perror("read");
+            return ;
+        }
+        i++;
+    }
     
-	bzero(&ph, sizeof(ph));
-	read(fd, &ph, sizeof(ph));
-	char directory[100];
-	bzero(directory, 100);
-    
-	for (int j = 0; j < ft_strlen(ph.name); j++) {
-		directory[j] = ph.name[j];
-	}
     while (i < nb_blocks - 2){
         bzero(&ph, sizeof(ph));
         read(fd, &ph, sizeof(ph));
-		if (!strncmp(directory, ph.name, ft_strlen(directory)))
-			printf("%s\n", &ph.name[ft_strlen(directory)]);
+        if (!strncmp(ph.name, ph.name, ft_strlen(ph.name)))
+            printf("> %s\n",ph.name);
         i++;
     }
 }
 
 int main(int argc, char * argv[]){
+    if (argc != 2){
+        perror("Wrong number of arguments\nUsage: ./afficher_repertoire <fichier.tar>");
+        return 1;
+    }
 
     int fd = open(argv[1], O_RDONLY);
-    afficher_repertoire(fd, 10);
+    afficher_repertoire(fd, 0);
     close(fd);
 
     return 0;
