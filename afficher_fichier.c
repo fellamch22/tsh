7c4fc9656303c39f01b8f9aa8fac15eaeb154aeb
Li-FangSu <su.lifang@outlook.com>
2020-10-14 23:56:37 +0200

    Update afficher_fichier

--- afficher_fichier.c
+++ afficher_fichier.c
@@ -6,58 +6,46 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-off_t ft_strlen(char *s) {
-	off_t i = 0;
-	while(s[i])
-		i++;
-	return 1;
-}
-
 void afficher_fichier(int fd, off_t position){
     
-	if (fd >= 0)
-	{
-		char buff[512];
+    if (fd >= 0){
+        char buff[BLOCKSIZE];
 
-		off_t i = 0;
-		while (i < position)
-		{
-			bzero(buff, sizeof(buff));
-			if (read(fd, buff, sizeof(buff)) == -1) {
-                perror("read\n");
-				return ;
-			}
-			i++;
-		}
-		if (write(1, buff, sizeof(buff)) == -1)
-		{
-            perror("write : ");
-			return ;
-		}
-	}
-	else {
-        perror("bad file descriptor : ");
+        off_t i = 0;
+
+        while (i < position){
+            bzero(buff, sizeof(buff));
+            if (read(fd, buff, sizeof(buff)) == -1) {
+                perror("read");
+                return ;
+            }
+            i++;
+        }
+        if (write(1, buff, sizeof(buff)) == -1){
+            perror("write");
+            return ;
+        }
+    }
+    else perror("bad file descriptor");
 }
 
-int main(int argc, char * argv[])
-{
-    if (argc != 2)
-    {
-        perror("Wrong number of arguments : ");
+int main(int argc, char * argv[]){
+    if (argc != 2){
+        perror("Wrong number of arguments\nUsage: ./afficher_fichier <fichier>\n");
         return 1;
     }
 
     int fd = open(argv[1], O_RDONLY);
-	if (fd < 0){
-        perror("write : open");
-		return -1;
-	}
-    afficher_fichier(fd, 3);
-    
+    if (fd < 0){
+        perror("open");
+        return -1;
+    }
+
+    afficher_fichier(fd, 1);
     if (close(fd) == -1){
-		perror("close : ");
-		return -1;
-	}
+        perror("close");
+        return -1;
+    }
 
     return 0;
 }
