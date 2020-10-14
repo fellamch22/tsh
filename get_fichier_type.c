68cc45819022032f035bb2aa4182d79bb230d662
Li-FangSu <su.lifang@outlook.com>
2020-10-14 23:37:58 +0200

    Update les autre types du fichier

--- get_fichier_type.c
+++ get_fichier_type.c
@@ -7,31 +7,50 @@
 #include <fcntl.h>
 
 char get_fichier_type(int fd, char *chemin){
+    
     struct stat buff;
     fstat(fd, &buff);
     int nb_blocks = (buff.st_size + 512 - 1) / 512;
-    //nb blocks exitant dans le fichier.tar
-    
     struct posix_header ph;
 
-    int i = 0;
-    int j = 0;
-    while (i < nb_blocks - 2){
+    int i=0;
+    while (i < nb_blocks - 2)
+    {
         bzero(&ph, sizeof(ph));
-        j = read(fd, &ph, sizeof(ph));
-        if (j == -1){
-            perror("read\n");
-            return ;
-        }
+        read(fd, &ph, sizeof(ph));
 
-        if (!strcmp(ph.name, chemin)) {
-            if (ph.typeflag == '0')
-                printf("Le chemin mene a un fichier");
-            else if (ph.typeflag == '5')
-                printf("Le chemin mene a un directory");
-            else
-                printf("Le chemin mene a un autre type");
-            return (ph.typeflag);
+        
+         if (!strcmp(ph.name, chemin)) {
+             switch(ph.typeflag){
+                case '0' :
+                            printf("[%c] Le chemin mene a un Fichier\n",ph.typeflag);
+                            break;
+                case '1' :
+                            printf("[%c] Le chemin mene a un Lien materiel\n",ph.typeflag);
+                            break;
+                case '2' :
+                            printf("[%c] Le chemin mene a un Lien symbolique\n",ph.typeflag);
+                            break;
+                case '3' :
+                            printf("[%c] Le chemin mene a un Fichier special caractere\n",ph.typeflag);
+                            break;
+                case '4' :
+                            printf("[%c] Le chemin mene a un Fichier special bloc\n",ph.typeflag);
+                            break;
+                case '5' :
+                            printf("[%c] Le chemin mene a un Repertoire\n",ph.typeflag);
+                            break;
+                case '6' :
+                            printf("[%c] Le chemin mene a un Tube nomme\n",ph.typeflag);
+                            break;
+                case '7' :
+                            printf("[%c] Le chemin mene a un Fichier contigu\n",ph.typeflag);
+                            break;
+                default :
+                            printf("[%c] Le chemin mene a un autre type\n",ph.typeflag);
+                            break;
+             }
+             return (ph.typeflag);
         }
         i++;
     }
@@ -39,15 +58,17 @@ char get_fichier_type(int fd, char *chemin){
     return 0;
 }
 
-int main(int argc, char * argv[]){
-    if (argc != 3){
-        perror("Wrong number of arguments : ");
+int main(int argc, char * argv[])
+{
+    if (argc != 3)
+    {
+        printf("Wrong number of arguments\nUsage: ./listar <file.tar> <chemin>\n");
         return 1;
     }
 
     int fd = open(argv[1], O_RDONLY);
-    char ret = get_fichier_type(fd, argv[2]);
-    
+    get_fichier_type(fd, argv[2]);
+   
     close(fd);
 
     return 0;
