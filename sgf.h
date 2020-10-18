8554b630d26241fea69b4f254b6edd050a3fd86c
fella mechouar <hf_mechouar@esi.dz>
2020-10-18 11:05:54 +0200

    header de la bibliotheque sgf

--- sgf.h
+++ sgf.h
@@ -3,8 +3,8 @@
 #define SGF_H
 #include "tar.h"
 
-int fileToBlocks( int fd , char * filename ,char * contenu );
-void addFile( int fd , char * contenu , int nb_blocks , off_t position);
+char * fileToBlocks( int fd , char * filename ,int * nb_blocks );
+void addFile( int fd, int fd1, char * src_filename ,off_t position);
 
 
 #endif
