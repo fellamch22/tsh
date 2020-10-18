f082f5316516c2a3368da4eb6332fc4866cca553
sbadji <sidybadji935@gmail.com>
2020-10-18 23:28:50 +0200

    la fonction delete_repertoire

--- sgf.c
+++ sgf.c
@@ -5,6 +5,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #include"tar.h"
 
 off_t trouve(int fd, char *filename){
@@ -38,7 +40,6 @@ void delete_fichier(int fd, char *filename){
  	if(position == -1){
    		perror("Fichier n'existe pas dans Fichiers.tar");
     	exit(1);
-	
   	}else{
 		if(fstat(fd,&s) == -1){
    		 	perror(" fstat failed ");
@@ -66,11 +67,69 @@ void delete_fichier(int fd, char *filename){
   	}
 }
 
+void delete_repertoire(int fd, char *filename){
+	size_t taille;
+	off_t position;		
+	struct stat s, stat_entry;
+	struct dirent *entry;
+	DIR *dir;
+	char *chemin;
+
+	position = trouve(fd, filename);
+	if(position == -1){
+		perror("Repertoire n'existe pas dans Fichiers.tar");
+    	exit(1);
+	else{
+		delete_fichier(fd, filename);
+		// si ton trouve un repertoire dans le fichier.tar 
+		fstat(fd, &s);
+		if(S_ISDIR(s.st_mode) == 0){
+			perror("Repertoire ");
+			exit(1);
+		}
+		if((dir = opendir(filename)) == NULL){
+			perror("Ouverture Repertoire ");
+			exit(1);
+		}
+		taille = strlen(filename);
+		while((entry = readdir(dir)) != NULL){
+			if(!strcmp(entry -> d_name, ".") || !strcmp(entry -> d_name, ".."))
+				continue;
+				
+
+			chemin = calloc(taille + strlen(entry -> d_name) + 1, sizeof(char));
+			strcpy(chemin, filename);
+			strcat(chemin, "/");
+			strcat(chemin, entry -> d_name);
+
+			fstat(fd, &stat_entry);
+
+			if(S_ISDIR(stat_entry.st_mode) != 0){
+				delete_repertoire(fd, filename);
+				continue;
+			}
+
+			if(unlink(chemin) == 0)
+				printf("Supprimer fichier : %s\n", chemin);
+			else
+				printf(" Echec suppression fichier  : %s\n", chemin);
+		
+			free(chemin);
+		}
+
+		if(rmdir(filename) == 0)
+			printf("Supprimer repertoire : %s\n", filename);
+		else	
+			printf("Echec suppression repertoire : %s\n", filename);
+		closedir(dir);
+	}
+}
+
 int main(int argc , char **argv){
 	int fd = open(argv[1], O_RDWR);
 	//printf("%ld\n" ,trouve(fd, argv[2]));
-	delete_fichier(fd, argv[2]);
+	//delete_fichier(fd, argv[2]);
+	delete_repertoire(fd, argv[2]);
 	close(fd);
 	return 0;
 }
-
