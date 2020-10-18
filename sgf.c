f09cfe5c5b5e5a33c374a5c42bd44e569d72f37e
fella mechouar <hf_mechouar@esi.dz>
2020-10-18 13:08:09 +0200

    correction du fonctionnement des fonctions deletefile et trouv , Test de suppression fonctionne tres bien

--- sgf.c
+++ sgf.c
@@ -11,59 +11,120 @@
 
 
 off_t trouve(int fd, char *filename){
+
   int filesize = 0;
-  lseek(fd, 0, SEEK_SET);
   struct posix_header p;
+
+  lseek(fd, 0, SEEK_SET);
+
+
+
   if(fd < 0){
+
     perror("Fichier n'existe pas");
     exit(1);
   }
 
   read(fd, &p, BLOCKSIZE);
+
   while(p.name[0] != '\0' ){
-    if(strcmp(p.name , filename) == 0){
-      sscanf(p.size, "%o", &filesize);
-      int diff = ((filesize + BLOCKSIZE - 1)/BLOCKSIZE)*BLOCKSIZE;
-      return  lseek(fd, diff, SEEK_CUR);
-    }
-     
-    read(fd, &p, BLOCKSIZE);
-  }
+
+   	 if(strcmp(p.name , filename) == 0){
+      		
+      		return  lseek(fd,-512, SEEK_CUR);
+
+    	}else{
+    	 
+		lseek(fd,((filesize + BLOCKSIZE - 1)/BLOCKSIZE)*BLOCKSIZE,SEEK_CUR);
+   	 	read(fd, &p, BLOCKSIZE);
+
+	}
+
+}
+  
   return  -1;
 }
 
 
 void delete_fichier(int fd, char *filename){
-  int filesize = 0;
-  int position = lseek(fd, 0, SEEK_SET);
-  struct posix_header p;
-  if(fd < 0){
-    perror("Fichier n'existe pas dans Fichiers.tar");
-    exit(1);
+
+	int filesize = 0;
+
+	off_t position , pos_decalage;
+
+	off_t diff;
+	struct stat s ;
+	struct posix_header p;
+
+  	
+  	position = trouve(fd, filename);
+
+ 	 if(position == -1){
+
+   		 perror("Fichier n'existe pas dans Fichiers.tar");
+    		exit(1);
+	
+  	}else{
+
+
+
+		if(fstat(fd,&s) == -1){
+
+			
+   		 	perror(" fstat failed ");
+    			exit(1);
+	
+		}
+
+	
+	// tete de lecture se trouve deja sur position
+
+		if(read(fd,&p,BLOCKSIZE) < 0){
+
+			
+   		 	perror(" read failed ");
+    			exit(1);
+	
+		}
+
+		sscanf(p.size,"%o",&filesize);
+
+		pos_decalage = lseek(fd,((filesize % 512) == 0)? filesize : ((filesize + BLOCKSIZE - 1)/BLOCKSIZE)*BLOCKSIZE , SEEK_CUR);
+
+		diff = s.st_size - pos_decalage;
+		char  decalage[diff];
+
+
+		if(read(fd,decalage,diff) < 0){
+
+			
+   		 	perror(" read failed ");
+    			exit(1);
+	
+		}
+
+		if(lseek(fd,position,SEEK_SET) == -1){
+
+			
+   		 	perror(" lseek failed ");
+    			exit(1);
+	
+		}
+		
+
   }
-  trouve(fd, filename);
-  if(trouve(fd, filename) == -1){
-    perror("Fichier n'existe pas dans Fichiers.tar");
-    exit(1);
-  }else{
-    remove(filename);
-    position -= BLOCKSIZE;
-    read(fd, &p, BLOCKSIZE);
-    while(p.name[0] != '\0'){
-      printf("fichier %s de type %c et de  taille : %s\n", p.name, p.typeflag, p.size);
-      sscanf(p.size, "%o", &filesize);
-      int diff = ((filesize + BLOCKSIZE - 1)/BLOCKSIZE)*BLOCKSIZE;
-      lseek(fd, diff, SEEK_CUR);
-      read(fd, &p, BLOCKSIZE);
-    }
-  }
+
 }
 
-int main(int argc , char **argv){
-  int fd = open(argv[1], O_RDONLY);
 
-  // printf("%ld\n", trouve(fd, argv[2]));
-  delete_fichier(fd, argv[2]);
-  return 0;
+int main(int argc , char **argv){
+
+	int fd = open(argv[1], O_RDWR);
+
+	delete_fichier(fd, argv[2]);
+
+	close(fd);
+
+	return 0;
 }
 
