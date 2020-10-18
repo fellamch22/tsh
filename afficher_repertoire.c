508209ba73db82a008c36561cf709470470207f6
Li-FangSu <su.lifang@outlook.com>
2020-10-18 20:14:45 +0200

    Update afficher_repertoire.c

--- afficher_repertoire.c
+++ afficher_repertoire.c
@@ -14,44 +14,44 @@ int ft_strlen(char *s) {
 }
 
 void afficher_repertoire(int fd, off_t position){
-    if (fd == -1){
-        perror("open");
-    }
-
     struct stat buff;
     fstat(fd, &buff);
     // Ici on calcule le nombre de blocks qu'il y a dans le fichier.tar
-    // tel que nbBlock * BLOCKSIZE = taille du fichier
-    // <==> nbBlock = taille de fichier / BLOCKSIZE
-
+    // tel que nbBlock * 512 = taille du fichier
+    // <==> nbBlock = taille de fichier / 512
     int nb_blocks = (buff.st_size + BLOCKSIZE - 1) / BLOCKSIZE;
     struct posix_header ph;
 
     off_t i = 0;
     while (i < position){
-        bzero(&ph, sizeof(ph));
+        memset(&ph, 0, sizeof(ph));
         if (read(fd, &ph, sizeof(ph)) == -1) {
-            perror("read");
+            perror("read\n");
             return ;
         }
         i++;
     }
+    memset(&ph, 0, sizeof(ph));
+    read(fd, &ph, sizeof(ph));
+    char directory[sizeof(ph.name)];
+    memset(&directory, 0, sizeof(ph.name));
+    
+    for (int j = 0; j < ft_strlen(ph.name); j++) {
+        directory[j] = ph.name[j];
+    }
     
     while (i < nb_blocks - 2){
-         bzero(&ph, sizeof(ph));
-         read(fd, &ph, sizeof(ph));
-         if (!strncmp(ph.name, ph.name, ft_strlen(ph.name)))
-             printf(" [%c] > %s\n",ph.typeflag, ph.name);
-         i++;
-     }
+        memset(&ph, 0, sizeof(ph));
+        read(fd, &ph, sizeof(ph));
+        if (!strncmp(directory, ph.name, ft_strlen(directory))){
+            printf("%s\n", &ph.name[ft_strlen(directory)]);
+        }
+        i++;
+    }
 }
 
-int main(int argc, char * argv[]){
-    if (argc != 2){
-        perror("Wrong number of arguments\nUsage: ./afficher_repertoire <fichier.tar>");
-        return 1;
-    }
-
+int main(int argc, char * argv[])
+{
     int fd = open(argv[1], O_RDONLY);
     afficher_repertoire(fd, 0);
     close(fd);
