7b5cacb5a039d9bb8b0a113332ef8d85c768b47f
fella mechouar <hf_mechouar@esi.dz>
2020-12-27 04:03:04 +0100

    fonction qui analyse le chemin et detecte les tarballs

--- syntaxe.c
+++ syntaxe.c
@@ -3,137 +3,121 @@
 #include <pwd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "syntaxe.h"
 
 
+/* analyse le chemin absolu donne en parametre 
+
+  si le chemin mene a un tarball elle retourne  le chemin a l'interieur du tarball et le descripteur du tarball
+  sinon elle retourne null et -1 dans fd
+
+*/
+char * analyser_path ( char * path , int * fd ){
 
 
-int analyser_src_dest (char * pwd ,char * source , char* destination , int* is_srctar , int* is_destar , int*
-is_srcfile , int* is_destdir, char * srcpart1 ,char* srcpart2,char* destpart1 ,char* destpart2 ){
+	char path_save [100];
+	char path_vers_tarball[100];
+	char * path_interieur_tarball ;
+	char * word ;
+
+	int trouv_tar =0 ;
+
+	// initialisation 
+	memset(path_save,'\0',100);
+	path_interieur_tarball = malloc(100);
+	memset(path_interieur_tarball,'\0',100);
+	memset(path_vers_tarball,'\0',100);
 
 
-/* ne pas oublier de liberer l'espace memoire */
-int src_len = -1;
-int part1 ;
-char * strbuf;
-char * srccopie [100];
-char * destcopie [100];
-char srcbuf[100];
-char destbuf[100];
-srcpart1 = malloc(100);
-srcpart2 = malloc(100);
-destpart1 = malloc(100);
-destpart2 = malloc(100);
-memset(srcpart1,'\0',100);
-memset(srcpart2,'\0',100);
-memset(destpart1,'\0',100);
-memset(destpart2,'\0',100);
-strcpy(srccopie,source);
-strcpy(destcopie,source);
+	strcpy(path_save,path);
 
-/** analyser source **/
+   // analyse du chemin
+	word = strtok(path_save,"/");
 
+	while ( word != NULL )
+	{
+		printf(" %s \n ", word);
+		if(strlen(word) > 4 && (path_interieur_tarball[0] =='\0')){
+			// tester si le mot contient une extension .tar 
 
+			if(strcmp(word+strlen(word)-4,".tar") == 0 ) trouv_tar = 1	;
+			
+			
+			strcat(path_vers_tarball,"/");
+			strcat(path_vers_tarball,word);
 
-// parcourir source1  pour detecter si il ya un .tar dans le chemin et mettre a jour
+		}else{
 
-strbuf = strtok(source,"/");
-part1 = 1 ;
+			if(trouv_tar){ // on met le mot dans le chemin interne
 
-while ( strbuf != NULL ){
+				if(path_interieur_tarball[0]=='\0'){ // si c'est le premier mot
 
-    if( strlen(strbuf) > 4){ // pour voir si il termine par .tar
+					strcpy(path_interieur_tarball,word);
 
-        if( strncmp(strbuf[strlen(strbuf)-4],".tar",4) == 0 ){
+				}else// il existe deja un mot
+				{
+					strcat(path_interieur_tarball,"/");
+					strcat(path_interieur_tarball,word);
+				}
+				
 
-			(*is_srctar) = 1;
-			if(src_len != -1) snprintf(srcpart1,src_len,"%s",pwd); 
-			src_len = -1 ;
-			strcat(srcpart1,"/");
-			strcat(srcpart1,strbuf);
-			//free(strbuf); - to check
+			}else{// on met dans le chemin vers le tarball
 
-			part1 = 0 ;
-		}else if( part1){ // on remplie la premiere partie de source avant de rencontrerun .tar
+				if(path_vers_tarball[0] == '\0'){ // si c'est le premier mot
 
-				if( strcmp(srcpart1,"") != 0 ) strcat(srcpart1,"/");
+					strcpy(path_vers_tarball,"/");
+					strcat(path_vers_tarball,word);
 
-				if(strcmp(strbuf,"~")==0){ // chemin qui commence par tilde non acceptable
-  				/* on met dans srcpart1 le chemin forme par home/username/ , car les fonctions open et read n'acceptent pas
-   				le cehmin qui commencent par tilde */
-
-   				sprintf(srcpart1,"/home/%s/",getpwuid(getuid())->pw_name);
-
-				}else if (strcmp(strbuf,".") == 0){
-			 	// chemin relatif qui commence par '.' , on le remplace par le chemin absolu du repertoire courant
-
-				 sprintf(srcpart1 ,"%s",pwd);
-
-				}else if( strncmp(source,"..",2) == 0){ 
-
- 					// chemin relatif qui commence par '.' , on le remplace par le chemin absolu du repertoire courant
-					// on retire le dernier repetoire du chemin courant --> need to find a solution
-					// je parcours src depuis la fin et je decrmente un compteur
-					// apres je copie avec snprintf
-					if ( src_len == -1 ) src_len = strlen(pwd) - 1;
-
-  					while(pwd[src_len ] != '/')  src_len -- ;
-	
-				}else{
-		
-						if(src_len != -1) snprintf(srcpart1,src_len,"%s/",pwd); 
-						src_len = -1 ;
-						strcat(srcpart1,strbuf);
+				}else// il existe deja un mot
+				{
+					strcat(path_vers_tarball,"/");
+					strcat(path_vers_tarball,word);
 				}
 
-		}else{ // on remplie la deuxieme partie de source apres recontrer un .tar
-
-				if( strcmp(srcpart2,"") != 0 ) strcat(srcpart2,"/");
-				strcat(srcpart2,strbuf);
-
+			}
 		}
+
+		// obtenir le mot suivant
+		word = strtok(NULL,"/");
 		
 	}
-
-	strbuf = strtok(source,"/");
-
- 
-}
-
-if(src_len != -1) snprintf(srcpart1,src_len,"%s/",pwd); 
-src_len = -1 ;
-
-// si partie 2 est remplie , on verifie la fin de source ( pour ne pas negliger le '/' de la fin  d'un repertoire dans un .tar )
-if(!part1 && srccopie[strlen(srccopie)-1] == '/') strcat(srcpart2,"/");
+	
+	if( trouv_tar ){// si on est dans un tar , 
 
 
-// ida kan tar diri open  w hawssi b getfichiertype
-if( (*is_srctar) ){
-   // je vois seulement la fin si c'est / c'est un repertoire , else it's file 
-   // (je ne teste pas avec un open et trouve pour diminuer le cout )
-   is_srcfile = (srcpart2[strlen(srcpart2)-1] == '/') ? 0 : 1;
+		//si l'interieur different du vide on verifie le slash '/'  a la fin du path (celui des repertoires)
 
-}else{
-   // source n'et pas un tar , on fait un stat 
-   // ida makach tar diri stat direct w djibi type tae hedek lfichier
-   struct stat s ;
-   if ( stat(srcpart1,&s)== -1){
-       perror(" argument incorrect ");
-       exit(1);
-   }
+		if( (path_interieur_tarball[0] !='\0') && (path[strlen(path)-1] == '/') )
+				 strcat(path_interieur_tarball,"/");
 
-   is_srcfile = (S_ISDIR(s.st_mode))? 0 : 1;
 
-{
 
-// analyser  la destination 
+		// ouvrir le fichier ( en lecture et ecriture)
 
-// faire de meme avec la destination
+		(*fd) = open(path_vers_tarball,O_RDWR);
+
+	}else{
+		// si on est pas dans un tar
+		// mettre fd a -1 
+	 	(*fd) = -1 ;
+	}
+
+	return path_interieur_tarball;
+
 
 }
 
-// strtok
 
-	return 0;
+
+int main(int argc, char* argv[]){
+
+	int fd = -1 ;
+	printf("le chemin : %s \n",analyser_path("/home/fella/Desktop/toto.tar/a/",&fd));
+	printf("%d \n",fd);
+    if( fd != -1 ) close(fd);
+    return 0;
 }
--- syntaxe.h
+++ syntaxe.h
@@ -3,8 +3,7 @@
 #include "syntaxe.h"
 
 
-int analyser_src_dest (char * pwd ,char * source , char* destination , int* is_srctar , int* is_destar , int*
-is_srcfile , int* is_destdir, char * srcpart1 ,char* srcpart2,char* destpart1 ,char* destpart2 )
+char * analyser_path ( char * path , int * fd );
 
 
 #endif
